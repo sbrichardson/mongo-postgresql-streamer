a4d7a50b64330dcf12496f861b9038ee020f8768
Houssem BELHADJ AHMED <houssem@belhadjahmed.com>
2018-10-11 10:07:03 +0200

    wip

--- src/main/java/com/malt/mongopostgresqlstreamer/StreamerApplication.java
+++ src/main/java/com/malt/mongopostgresqlstreamer/StreamerApplication.java
@@ -8,18 +8,21 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 
 import java.util.Optional;
 
 @SpringBootApplication
 @Slf4j
+@Profile("production")
 public class StreamerApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(StreamerApplication.class, args);
     }
 
-    @Value("${mongo.connector.forcereimport:false}")
+    @Value("${mongo.connector.forcereimport:true}")
     private boolean forceReimport;
 
     @Autowired
